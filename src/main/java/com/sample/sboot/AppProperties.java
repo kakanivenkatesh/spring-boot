a22af655da9a5adcdd6ea8e1e87865a62d2261c3
venkateswarluk <venkateswarlu.kakani@imaginea.com>
2019-01-27 21:10:42 +0530

    spring boot learning

--- src/main/java/com/sample/sboot/AppProperties.java
+++ src/main/java/com/sample/sboot/AppProperties.java
@@ -1,4 +1,4 @@
-package com.sample.model;
+package com.sample.sboot;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
--- src/main/java/com/sample/sboot/DemoApplication.java
+++ src/main/java/com/sample/sboot/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.sample.model;
+package com.sample.sboot;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
